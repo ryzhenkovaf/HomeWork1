05d65fda15d9bb6810498ebf2713c14208d51477
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-10 20:38:24 +0700

    add #6

---
+++ #6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
