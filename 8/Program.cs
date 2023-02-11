bbc18c303457a2252b9f92f40977386a115e1111
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-11 14:31:44 +0700

    add 8

---
+++ 8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+        count = count + 1;
+    }
+    else
+    {
+        count = count + 1;
+    }
+}
